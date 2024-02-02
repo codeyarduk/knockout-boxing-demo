82e829c7400568bbe37e6846bf9318ea30bfbc42
davidwrossiter <davewrossiter@gmail.com>
2024-02-02 12:14:20 +0000

    removed the starter code and added the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'knockout-brown': '#1D1107',
+      },
+      fontFamily: {
+        Pridi: ['Pridi', 'serif']
+      }
+    },
   },
   plugins: [],
 }
